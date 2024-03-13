832bccbe26dd9a5b3114e55b6c1df309c7b1fe22
danilolan <didoherculano@gmail.com>
2024-03-13 18:24:06 -0300

    feat: useTable hook

--- src/app/list/components/table.tsx
+++ src/app/list/components/table.tsx
@@ -23,14 +23,22 @@ import { useState } from "react";
 import useSWR from "swr";
 import { AllCustomersDTO } from "@/types/dtos";
 import FilterInput from "./filter-input";
+import useTable from "../hooks/useTable";
 
 export default function Table() {
-  const [filterValue, setFilterValue] = useState("");
-  const [filterKey, setFilterKey] = useState("");
-
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
-  const [totalCount, setTotalCount] = useState(0);
+  const {
+    filterValue,
+    setFilterValue,
+    filterKey,
+    setFilterKey,
+    page,
+    rowsPerPage,
+    totalCount,
+    handleChangePage,
+    handleChangeRowsPerPage,
+    updateTotalCount,
+    TablePaginationActions,
+  } = useTable();
 
   const { data, isLoading } = useSWR<AllCustomersDTO>(
     `${process.env.NEXT_PUBLIC_SERVER_URL}/customers?page=${
@@ -39,94 +47,11 @@ export default function Table() {
     async (url: string) => {
       const response = await fetch(url);
       const data: AllCustomersDTO = await response.json();
-      setTotalCount(data.total);
+      updateTotalCount(data.total);
       return data;
     }
   );
 
-  const handleChangePage = (
-    event: React.MouseEvent<HTMLButtonElement> | null,
-    newPage: number
-  ) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  function TablePaginationActions(props: TablePaginationActionsProps) {
-    const theme = useTheme();
-    const { count, page, rowsPerPage, onPageChange } = props;
-
-    const handleFirstPageButtonClick = (
-      event: React.MouseEvent<HTMLButtonElement>
-    ) => {
-      onPageChange(event, 0);
-    };
-
-    const handleBackButtonClick = (
-      event: React.MouseEvent<HTMLButtonElement>
-    ) => {
-      onPageChange(event, page - 1);
-    };
-
-    const handleNextButtonClick = (
-      event: React.MouseEvent<HTMLButtonElement>
-    ) => {
-      onPageChange(event, page + 1);
-    };
-
-    const handleLastPageButtonClick = (
-      event: React.MouseEvent<HTMLButtonElement>
-    ) => {
-      onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-    };
-
-    return (
-      <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-        <IconButton
-          onClick={handleFirstPageButtonClick}
-          disabled={page === 0}
-          aria-label="first page"
-        >
-          {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
-        </IconButton>
-        <IconButton
-          onClick={handleBackButtonClick}
-          disabled={page === 0}
-          aria-label="previous page"
-        >
-          {theme.direction === "rtl" ? (
-            <KeyboardArrowRight />
-          ) : (
-            <KeyboardArrowLeft />
-          )}
-        </IconButton>
-        <IconButton
-          onClick={handleNextButtonClick}
-          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-          aria-label="next page"
-        >
-          {theme.direction === "rtl" ? (
-            <KeyboardArrowLeft />
-          ) : (
-            <KeyboardArrowRight />
-          )}
-        </IconButton>
-        <IconButton
-          onClick={handleLastPageButtonClick}
-          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-          aria-label="last page"
-        >
-          {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
-        </IconButton>
-      </Box>
-    );
-  }
   return isLoading ? (
     <CircularProgress />
   ) : (
@@ -149,7 +74,7 @@ export default function Table() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {data?.customers.map(({ name, email, phone, position, id }) => (
+            {data?.customers?.map(({ name, email, phone, position, id }) => (
               <TableRow
                 key={id}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
