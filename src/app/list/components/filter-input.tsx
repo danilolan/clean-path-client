600ad9f957253db427dc603710ec0920b867c336
danilolan <didoherculano@gmail.com>
2024-03-13 22:20:43 -0300

    fix: input rerender

--- src/app/list/components/filter-input.tsx
+++ src/app/list/components/filter-input.tsx
@@ -8,52 +8,72 @@ import {
 } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { useFilter } from "../context/useFilter";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { error } from "console";
 
-type Props = {
-  filterValue: string;
-  setFilterValue: (value: string) => void;
-  filterkey: string;
-  setFilterkey: (value: string) => void;
+type Inputs = {
+  value: string;
 };
 
 export default function FilterInput() {
   const { filterValue, setFilterValue, filterKey, setFilterKey } = useFilter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+    reset,
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = ({ value }) => {
+    setFilterValue(value);
+  };
+
+  console.log(errors);
   return (
-    <div className="w-full bg-white rounded shadow p-4">
-      <div className="grid grid-cols-[70%,30%]">
-        <TextField
-          value={filterValue}
-          onChange={(e) => setFilterValue(e.target.value)}
-          label="Search"
-          size="small"
-          className="w-full"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon className="text-darkGrey left-2" />
-              </InputAdornment>
-            ),
-          }}
-        />
-        <div className="pl-4">
-          <FormControl fullWidth size="small">
-            <InputLabel id="demo-simple-select-label">Field</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={filterKey}
-              label="Age"
-              onChange={(e) => {
-                setFilterKey(e.target.value);
+    <div className="grid grid-cols-[70%,30%] w-full bg-white rounded shadow p-4">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="value"
+          control={control}
+          defaultValue={filterValue}
+          render={({ field, fieldState }) => (
+            <TextField
+              {...field}
+              {...fieldState}
+              error={!!errors.value}
+              label="Search"
+              size="small"
+              className="w-full"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon className="text-darkGrey left-2" />
+                  </InputAdornment>
+                ),
               }}
-            >
-              <MenuItem value={"id"}>Id</MenuItem>
-              <MenuItem value={"name"}>Name</MenuItem>
-              <MenuItem value={"email"}>Email</MenuItem>
-              <MenuItem value={"phone"}>Phone</MenuItem>
-            </Select>
-          </FormControl>
-        </div>
+            />
+          )}
+        />
+      </form>
+      <div className="pl-4">
+        <FormControl fullWidth size="small">
+          <InputLabel id="demo-simple-select-label">Field</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={filterKey}
+            label="Age"
+            onChange={(e) => {
+              setFilterKey(e.target.value);
+            }}
+          >
+            <MenuItem value={"id"}>Id</MenuItem>
+            <MenuItem value={"name"}>Name</MenuItem>
+            <MenuItem value={"email"}>Email</MenuItem>
+            <MenuItem value={"phone"}>Phone</MenuItem>
+          </Select>
+        </FormControl>
       </div>
     </div>
   );
--- src/app/list/context/useFilter.tsx
+++ src/app/list/context/useFilter.tsx
@@ -1,6 +1,11 @@
-import React, { ReactNode, createContext, useContext, useState } from "react";
+import React, {
+  ReactNode,
+  createContext,
+  useContext,
+  useMemo,
+  useState,
+} from "react";
 
-// Define o tipo dos valores do contexto
 type FilterContextType = {
   filterValue: string;
   setFilterValue: (value: string) => void;
@@ -8,26 +13,38 @@ type FilterContextType = {
   setFilterKey: (key: string) => void;
 };
 
-// Cria o contexto com valores padrão
-const FilterContext = createContext<FilterContextType | undefined>(undefined);
-
 type FilterProviderProps = {
   children: ReactNode;
 };
 
-// Cria um Provider que encapsula a lógica de estado do filtro
+const defaultValue: FilterContextType = {
+  filterValue: "",
+  setFilterValue: () => {},
+  filterKey: "",
+  setFilterKey: () => {},
+};
+
+export const FilterContext = createContext<FilterContextType>(defaultValue);
+
 export const FilterProvider: React.FC<FilterProviderProps> = ({ children }) => {
   const [filterValue, setFilterValue] = useState("");
   const [filterKey, setFilterKey] = useState("");
 
-  const value = { filterValue, setFilterValue, filterKey, setFilterKey };
+  const value = useMemo(
+    () => ({
+      filterValue,
+      setFilterValue,
+      filterKey,
+      setFilterKey,
+    }),
+    [filterValue, filterKey]
+  );
 
   return (
     <FilterContext.Provider value={value}>{children}</FilterContext.Provider>
   );
 };
 
-// Hook customizado para usar o contexto de filtro
 export const useFilter = (): FilterContextType => {
   const context = useContext(FilterContext);
   if (context === undefined) {
